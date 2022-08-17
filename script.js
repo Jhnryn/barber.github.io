40cebc8c1310c5a218b663417a19647dfd0dc871
Jhnryn <109158469+Jhnryn@users.noreply.github.com>
2022-08-17 22:49:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var menuBtn = document.getElementById("menuBtn")
+var sidenav = document.getElementById("sidenav")
+var menu = document.getElementById("menu")
+
+sidenav.style.right = "-250px";
+menuBtn.onclick = function(){
+    if (sidenav.style.right == "-250px"){
+            sidenav.style.right = "0";
+            menu.src = "img/close.png";
+    }
+    else {
+        sidenav.style.right = "-250px";
+        menu.src = "img/menu.png";
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+    });
+
